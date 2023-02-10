58c9c0b4ca5d144fdaa66e47d03f7d8bb165d645
Emon Dewan <emondewan00>
2023-02-10 22:50:17 +0600

    sort using id section deone

--- javascript/index.js
+++ javascript/index.js
@@ -4,10 +4,10 @@ const milestoneData=JSON.parse(data).data;
 function displayList(){
     const list= document.getElementById("video-list");
     list.innerHTML=`${milestoneData.map(data=>{
-        return`<div class="milestone">
+        return`<div class="milestone" id="${data._id}">
         <div class="milestone-heading border-2 border-bottom">
-            <div class="checkbox pe-3">
-                <input type="checkbox" name="" id="">
+            <div class="checkbox pe-3" >
+                <input type="checkbox" name="" id="" onClick="doneList(this,${data._id})">
             </div>
             <div class="milestone-text" onClick="myFunction(this)" >
                 <h3>${data.name}</h3>
@@ -34,3 +34,37 @@ function myFunction(ele){
     }
     crouunt.classList.toggle("show");
 }
+
+
+function doneList(ele,id){
+    const listDiv=document.getElementById("hallo");
+    const list= document.getElementById("video-list");
+    const getEle=document.getElementById(id);
+    if(ele.checked){
+        list.removeChild(getEle);
+        listDiv.appendChild(getEle);
+        const doneModule=listDiv.children;
+        sortElement(doneModule,"hallo");
+    }else{
+        listDiv.removeChild(getEle)
+        list.appendChild(getEle);
+        const video=list.children;
+        sortElement(video,"video-list")
+    }
+}
+
+
+
+function sortElement(parent,section){
+    const listDiv=document.getElementById(section);
+    const sortTed=Array.from(parent).sort((a,b)=>{
+        return a.id - b.id
+    })
+    for (let x of sortTed){
+        listDiv.appendChild(x);
+    }
+}
+
+
+
+
